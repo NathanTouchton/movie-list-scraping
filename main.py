707431dd064cb6ded95d0cdfd2b7e142544b79b2
Nathan Touchton <ntouchton@protonmail.com>
2023-05-07 14:54:10 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ with open("index.html") as file:
     content = file.read()
 
 soup = BeautifulSoup(content, "html.parser")
-print(soup.title)
+a = soup.find_all(name="a")
+
+# for anchor in a:
+#     print(anchor.get("href"))
+
+company_website = soup.select_one("p a")
+
+print(soup.select("#name"))
